f393910b861a3607565b3e41ccdec6faf24e48da
Vitor Magalhães <vitor.magalhaes@ndrive.com>
2018-07-31 12:14:09 +0100

    creating shared framework for iOS

---
+++ AvePurchaseButton/AvePurchaseButton/AvePurchaseButton.h
@@ -0,0 +1,17 @@
+//
+//  AvePurchaseButton.h
+//  AvePurchaseButton
+//
+//  Created by Vitor Magalhães on 31/07/2018.
+//  Copyright © 2018 NDrive SA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AvePurchaseButton.
+FOUNDATION_EXPORT double AvePurchaseButtonVersionNumber;
+
+//! Project version string for AvePurchaseButton.
+FOUNDATION_EXPORT const unsigned char AvePurchaseButtonVersionString[];
+
+
